24a9853e7d49971090816f64d38f3a78956c2776
Abhishek-vamja <abhishekvamja2518@gmail.com>
2023-12-12 11:32:19 +0530

    Create Models.

--- video_player/video_app/models.py
+++ video_player/video_app/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+"""
+Model for video app.
+"""
 
-# Create your models here.
+from django.db import models
+from core.models import BaseModel
+from django.core.validators import FileExtensionValidator 
+from django.conf import settings
+
+class Category(BaseModel):
+    title = models.CharField(max_length=255,null=True)
+    slug = models.SlugField(null=True)
+    image = models.ImageField(upload_to='cat/image',null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Product(BaseModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    slug = models.SlugField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=255,blank=True,null=True)
+    image = models.ImageField(upload_to='image', null=True)
+    video = models.FileField(upload_to='videos',null=True,
+            validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self) -> str:
+        return f'{self.user} {self.title}'
