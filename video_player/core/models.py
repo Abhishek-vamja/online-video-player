273470d1907ceb9e97ffaa8a5bab4718be1e65df
Abhishek-vamja <abhishekvamja2518@gmail.com>
2023-12-12 11:30:12 +0530

    Make BaseModel.

--- video_player/core/models.py
+++ video_player/core/models.py
@@ -1,3 +1,19 @@
+"""
+Core model for apps.
+"""
 from django.db import models
 
-# Create your models here.
+import uuid
+
+class BaseModel(models.Model):
+    """Base Model."""
+    class Meta:
+        abstract = True
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
